e16393caab9e4d819493149009f33bac74b34a51
Patrick Snijder <76705330+psnijder@users.noreply.github.com>
2024-11-25 11:13:52 +0100

    Remove unused code and change variable names for consistency

--- script.js
+++ script.js
@@ -17,66 +17,50 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-/*
-function getHumanChoice() {
-    let humanChoice = "rock";
-    
-    while (true) {
-        humanChoice = prompt("Choose 'Rock', 'Paper' or 'Scissors'.").toLowerCase();
-
-        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-            return humanChoice;
-        }
-
-    alert("Invalid input, please try again.")
-    }
-}
-*/
-
 let computerScore = 0;
-let humanScore = 0;
+let playerScore = 0;
 
 const results = document.getElementById("results");
 const score = document.getElementById("score");
 const displayPlayerScore = document.getElementById("playerScore");
 const displayComputerScore = document.getElementById("computerScore");
 
-function playRound(computerChoice, humanChoice) {
-    if (computerChoice === humanChoice) {
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
         results.textContent = "Round is a tie!";
     } else if (computerChoice === "rock") {
-        if (humanChoice === "scissors") {
+        if (playerChoice === "scissors") {
             results.textContent = "Computer picked rock and won round!";
             computerScore++;
             displayComputerScore.textContent = computerScore;
         } else {
             results.textContent = "Computer picked rock. Human won round!";
-            humanScore++;
-            displayPlayerScore.textContent = humanScore;
+            playerScore++;
+            displayPlayerScore.textContent = playerScore;
         }
     } else if (computerChoice === "paper") {
-        if (humanChoice === "rock") {
+        if (playerChoice === "rock") {
             results.textContent = "Computer picked paper and won round!";
             computerScore++;
             displayComputerScore.textContent = computerScore;
         } else {
             results.textContent = "Computer picked paper. Human won round!";
-            humanScore++;
-            displayPlayerScore.textContent = humanScore;
+            playerScore++;
+            displayPlayerScore.textContent = playerScore;
         }
     } else if (computerChoice === "scissors") {
-        if (humanChoice === "paper") {
+        if (playerChoice === "paper") {
             results.textContent = "Computer picked scissors and won round!";
             computerScore++;
             displayComputerScore.textContent = computerScore;
         } else {
             results.textContent = "Computer picked scissors. Human won round!";
-            humanScore++;
-            displayPlayerScore.textContent = humanScore;
+            playerScore++;
+            displayPlayerScore.textContent = playerScore;
         }
     }
 
-    if (computerScore == 5 || humanScore == 5) {
+    if (computerScore == 5 || playerScore == 5) {
         rockButton.remove();
         paperButton.remove();
         scissorsButton.remove();
@@ -88,25 +72,6 @@ function playRound(computerChoice, humanChoice) {
         }
     }
 }
-
-/*
-function playGame() {
-    for (let round = 0; round < 5; round++) {
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
-
-        playRound(computerSelection, humanSelection);
-    }
-
-    if (computerScore === humanScore) {
-        return(alert(`The final score is: \nComputer: ${computerScore},\nHuman: ${humanScore}.\nThe game is a tie!`));
-    } else if (computerScore > humanScore) {
-        return(alert(`The final score is: \nComputer: ${computerScore},\nHuman: ${humanScore}.\nComputer won!`));
-    } else if (humanScore > computerScore) {
-        return(alert(`The final score is: \nComputer: ${computerScore},\nHuman: ${humanScore}.\nHuman won!`));
-    }
-}
-*/
 
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
